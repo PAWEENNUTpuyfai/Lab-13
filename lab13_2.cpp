54db459d56bb5952c58ed5e55590b0f4705b0455
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2023-01-10 15:35:31 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
 int main(){
     int x, y;
     cin >> x >> y;
     cout << gcd(x,y);
     return 0; 
+}
+
+int gcd(int n1,int n2){
+	if(n1%n2==0){
+		return n2;
+	}else{
+		return gcd(n2,n1%n2);
+	}
 }
